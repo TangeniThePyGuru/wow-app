4247fb52220e59b03c1ff60c12aa62210d7e9809
Tangeni <tangenishikomba@gmail.com>
2017-11-19 02:36:30 +0200

    login directive added

---
+++ app/scripts/directives/login.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name wowAppApp.directive:login
+ * @description
+ * # login
+ */
+angular.module('wowAppApp')
+  .directive('login', function () {
+    return {
+      template: '<div></div>',
+      restrict: 'E',
+      link: function postLink(scope, element, attrs) {
+        element.text('this is the login directive');
+      }
+    };
+  });
