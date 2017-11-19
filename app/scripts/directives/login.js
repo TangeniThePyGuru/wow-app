b3626c005826898d299956d36603971cb90846e7
Tangeni <tangenishikomba@gmail.com>
2017-11-19 05:54:16 +0200

    login directive created

--- app/scripts/directives/login.js
+++ app/scripts/directives/login.js
@@ -9,7 +9,7 @@
 angular.module('wowAppApp')
   .directive('login', function () {
     return {
-      template: '<div></div>',
+      templateUrl: '<div></div>',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
         element.text('this is the login directive');
